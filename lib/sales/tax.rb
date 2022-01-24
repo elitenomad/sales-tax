db08ea0aba37c0b0dcdcb1f6c69fedd7fff5e16f
pbalugari <pranava.balugari@PKS-LT-103.local>
2022-01-25 08:30:36 +1100

    Gateway class to the GEM, import all the classes from here

--- lib/sales/tax.rb
+++ lib/sales/tax.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 require_relative "tax/version"
+require_relative "tax/calculate"
+require_relative "tax/product"
+require_relative "tax/receipt"
+require_relative "tax/receipt_formatter"
+require_relative "tax/build_product"
+require_relative "tax/cli"
 
 module Sales
   module Tax
     class Error < StandardError; end
-    # Your code goes here...
+    class FileNotExistsError < StandardError; end
+    class FileFormatNotSupportedError < StandardError; end
+    class EmptyReceiptError < StandardError; end
   end
 end
